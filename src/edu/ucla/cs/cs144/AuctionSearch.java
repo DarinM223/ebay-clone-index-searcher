fdce8b0676188f7fb76c67a008fadd1c2aa29d83
Darin Minamoto <darinm223@gmail.com>
2015-02-13 01:10:00 -0800

    Started getXMLDataForItemId, probably won't compile yet

--- src/edu/ucla/cs/cs144/AuctionSearch.java
+++ src/edu/ucla/cs/cs144/AuctionSearch.java
@@ -35,6 +35,14 @@ import edu.ucla.cs.cs144.DbManager;
 import edu.ucla.cs.cs144.SearchRegion;
 import edu.ucla.cs.cs144.SearchResult;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.*;
+import javax.xml.transform.*;
+import java.io.StringWriter;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 public class AuctionSearch implements IAuctionSearch {
 	
 	public SearchResult[] basicSearch(String query, int numResultsToSkip, 
@@ -136,11 +144,226 @@ public class AuctionSearch implements IAuctionSearch {
 		try {
 			Connection conn = DbManager.getConnection(true);
 
+			PreparedStatement query = connection.prepareStatement("SELECT * FROM Item WHERE ItemID = ?");
+			query.setString(1, itemId);
+			ResultSet itemData = query.executeQuery();
+
+			itemData.first(); // get the first instance of item (best match)
+
+			if (itemData.getRow() != 0) {
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				org.w3c.dom.Document doc = builder.newDocument();
+
+				/*
+				 * Retrieves data from result
+				 * TODO: make sure that the data is properly encoded (like in currency format or in timestamp format)
+				 */
+
+				String userid = result.getString("Seller");
+				String name = result.getString("Name");
+				String currently = result.getString("Currently");
+				currently = getCurrencyString(currently);
+
+				String buyprice = result.getString("BuyPrice"); 
+				buyprice = getCurrencyString(buyprice);
+
+				String firstbid = result.getString("FirstBid");
+				firstbid = getCurrencyString(firstbid); 
+
+				String started = result.getString("Started"); 
+				started = getTimeString(started);
+
+				String ends = result.getString("Ends");
+				ends = getTimeString(ends);
+
+				String location = result.getString("Location");
+
+				boolean hasLatitudeLongitude;
+				String latitude, longitude;
+				try {
+					latitude = result.getString("Latitude");
+					longitude = result.getString("Longitude");
+					hasLatitudeLongitude = true;
+				} catch (SQLException e) {
+					hasLatitudeLongitude = false;
+				}
+
+				String country = result.getString("Country");
+				String description = result.getString("Description");
+
+				PreparedStatement query2 = connection.prepareStatement("SELECT * FROM Seller WHERE UserID = ?");
+				PreparedStatement query3 = connection.prepareStatement("SELECT * FROM ItemCategory WHERE ItemID = ?");
+				PreparedStatement query4 = connection.prepareStatement("SELECT COUNT(*) FROM Bid WHERE ItemID = ?");
+				PreparedStatement query5 = connection.prepareStatement("SELECT * FROM Bid WHERE ItemID = ?")
+
+				query2.setString(userid);
+				query3.setString(itemId);
+				query4.setString(itemId);
+				query5.setString(itemId);
+
+				result = query2.executeQuery();
+				result.first();
+				String rating = result.getString("Rating");
+
+				result = query3.executeQuery();
+				result.first();
+				ArrayList<String> categories = new ArrayList<>();
+				while (result.next()) {
+					categories.add(result.getString("Category"));
+				}
+
+				result = query4.executeQuery();
+				result.first();
+				String numberOFBids = result.getString("COUNT(*)");
+
+				/*
+				 * Builds DOM Tree
+				 */
+
+				Element root = doc.createElement("Item");
+				root.setAttribute("ItemID", itemId);
+				doc.appendChild(root);
+
+				Element elementName = doc.createElement("Name");
+				elementName.appendChild(doc.createTextNode(name));
+				root.appendChild(elementName);
+
+				for (String category : categories) {
+					Element elementCategory = doc.createElement("Category");
+					elementCategory.appendChild(doc.createTextNode(category));
+					root.appendChild(elementCategory);
+				}
+
+				Element elementCurrently = doc.createElement("Currently");
+				elementCurrently.appendChild(doc.createTextNode(currently));
+				root.appendChild(elementCurrently);
+
+				Element elementBuyPrice = doc.createElement("Buy_Price");
+				elementBuyPrice.appendChild(doc.createTextNode(buyprice));
+				root.appendChild(elementBuyPrice);
+
+				Element elementFirstBid = doc.createElement("First_Bid");
+				elementFirstBid.appendChild(doc.createTextNode(firstbid));
+				root.appendChild(elementFirstBid);
+
+				Element elementNumBids = doc.createElement("Number_of_Bids");
+				elementNumBids.appendChild(doc.createTextNode(numberOFBids));
+				root.appendChild(elementNumBids);
+
+				Element elementBids = doc.createElement("Bids");
+				result = query5.executeQuery();
+				while (result.next()) {
+					String bidUserID = result.getString("UserID");
+					String bidTime = result.getString("Time");
+					bidTime = getTimeString(bidTime);
+
+					// TODO: Convert to currency format?
+					String bidAmount = result.getString("Amount")	
+					bidAmount = getCurrencyString(bidAmount);
+
+					Element elementBid = doc.createElement("Bid");
+					PreparedStatement queryBid = conn.prepareStatement("SELECT * FROM Bidder WHERE UserID = ?");
+					queryBid.setString(1, bidUserID);
+
+					ResultSet bidResult = queryBid.executeQuery();
+					bidResult.first();
+
+					String bidUserRating = bidResult.getString("Rating");
+					String bidUserLocation = bidResult.getString("Location");
+					String bidUserCountry = bidResult.getString("Country");
+
+					Element elementBidUser = doc.createElement("Bidder");
+					elementBidUser.setAttribute("Rating", bidUserRating);
+					elementBidUser.setAttribute("UserID", bidUserID);
+
+					Element elementBidLocation = doc.createElement("Location");
+					elementBidLocation.appendChild(doc.createTextNode(bidUserLocation));
+					elementBidUser.appendChild(elementBidLocation);
+
+					Element elementBidCountry = doc.createElement("Country");
+					elementBidCountry.appendChild(doc.createTextNode(bidUserCountry));
+					elementBidUser.appendChild(elementBidCountry);
+
+					elementBid.appendChild(elementBidUser);
+
+					Element elementTime = doc.createElement("Time");
+					elementTime.appendChild(doc.createTextNode(bidTime));
+					elementBid.appendChild(elementTime);
+
+					Element elementAmount = doc.createElement("Amount");	
+					elementAmount.appendChild(doc.createTextNode(bidAmount));
+					elementBid.appendChild(elementAmount);
+
+					elementBids.appendChild(elementBid);
+				}
+
+				root.appendChild(elementBids);
+
+				Element elementLocation = doc.createElement("Location");
+
+				// TODO: I don't know what happens if only one attribute is not null like if longitude is null but latitude is not
+				// I also don't know if this is the correct way to check if its null
+				if (hasLatitudeLongitude && latitude != null && longitude != null) {
+					elementLocation.setAttribute("Latitude", latitude);
+					elementLocation.setAttribute("Longitude", longitude);
+				}
+
+				// TODO: I don't know if I have to escape characters b/c I'm using XML Docment builder instead of concating strings
+				elementLocation.appendChild(doc.createTextNode(location));
+
+				Element elementCountry = doc.createElement("Country");
+				elementCountry.appendChild(doc.createTextNode(country));
+				root.appendChild(elementCountry);
+
+				Element elementStarted = doc.createElement("Started");
+				elementStarted.appendChild(doc.createTextNode(started));
+				root.appendChild(elementStarted);
+
+				Element elementEnds = doc.createElement("Ends");	
+				elementEnds.appendChild(doc.createTextNode(ends));
+				root.appendChild(elementEnds);
+
+				Element elementSeller = doc.createElement("Seller");	
+				elementSeller.setAttribute("Rating"< rating);
+				elementSeller.setAttribute("UserID", userid);
+				root.appendChild(elementSeller);
+
+				TransformerFactory transFactory = TransformerFactory.newInstance();
+				Transformer transformer = transFactory.newTranformer();
+				// I don't know wtf this is lol
+				transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+				transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+				transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+				transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+				StringWriter writer = new StringWriter();
+				transformer.transform(new DOMSource(doc), new StreamResult(writer));
+				resultXML = writer.getBuffer().toString();
+			}
+
 			conn.close();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		return "";
+		return resultXML;
+	}
+
+	private String getTimeString(String timestamp) {
+		SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		SimpleDateFormat output = new SimpleDateFormat("MMM-dd-yy HH:mm:ss");
+		StringBuffer stringBuffer = new StringBuffer();
+
+		try {
+			Date d = input.parse(timestamp);
+			return "" + output.format(d);
+		} catch (Exception e) {
+			System.out.println(e);
+			return "";
+		} 
+	}
+
+	private String getCurrencyString(float num) {
+		return String.format("$%.2f", num);
 	}
 	
 	public String echo(String message) {
